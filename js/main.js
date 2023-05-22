500c9233049df2e8437919299d716e809315aebc
Dejorden <d.moerman@hotmail.nl>
2023-05-22 14:22:14 +0200

    add second filter

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let filters = document.getElementsByClassName("filter");
 let indieFilter = document.getElementById("checkbox-indie");
 
 indieFilter.onchange = function () {
-    if (filters[0].checked === true) {
+    if (indieFilter.checked === true) {
         for (let i = 0; i < allGames.length; i++) {
             if (allGames[i].dataset.category === "indie") {
                 allGames[i].style.display = "block";
@@ -15,6 +15,44 @@ indieFilter.onchange = function () {
     } else {
         for (let i = 0; i < allGames.length; i++) {
             if (allGames[i].dataset.category === "indie") {
+                allGames[i].style.display = "none";
+            }
+        }
+    }
+}
+
+//Open world filter
+let openWorldFilter = document.getElementById("checkbox-open-world");
+
+openWorldFilter.onchange = function () {
+    if (openWorldFilter.checked === true) {
+        for (let i = 0; i < allGames.length; i++) {
+            if (allGames[i].dataset.category === "open-world") {
+                allGames[i].style.display = "block"
+            }
+        }
+    } else {
+        for (let i = 0; i < allGames.length; i++) {
+            if (allGames[i].dataset.category === "open-world") {
+                allGames[i].style.display = "none";
+            }
+        }
+    }
+}
+
+//Action filter
+let actionFilter = document.getElementById("checkbox-action");
+
+actionFilter.onchange = function () {
+    if (actionFilter.checked === true) {
+        for (let i = 0; i < allGames.length; i++) {
+            if (allGames[i].dataset.category === "action") {
+                allGames[i].style.display = "block"
+            }
+        }
+    } else {
+        for (let i = 0; i < allGames.length; i++) {
+            if (allGames[i].dataset.category === "action") {
                 allGames[i].style.display = "none";
             }
         }
