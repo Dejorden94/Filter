1b8ed95785700a48f2625271c19e8d49dd68b2a3
Dejorden <d.moerman@hotmail.nl>
2023-05-22 13:43:49 +0200

    add id to filter

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ let allGames = document.getElementsByClassName("game");
 let filters = document.getElementsByClassName("filter");
 
 //Indie filter
-filters[0].onchange = function () {
+let indieFilter = document.getElementById("checkbox-indie");
+
+indieFilter.onchange = function () {
     if (filters[0].checked === true) {
         for (let i = 0; i < allGames.length; i++) {
             if (allGames[i].dataset.category === "indie") {
